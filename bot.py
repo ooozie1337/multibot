a0bf0ac47e5d3d7a6bc26b9293e4a57e201f84ee
ooozie1337 <jeevacation1337@gmail.com>
2026-06-19 23:03:54 +0300

    cleaned up the code and fixed some stuff

--- bot.py
+++ bot.py
@@ -1,19 +1,31 @@
+# Import all the nessecary packages
+
 from dotenv import load_dotenv
 import discord, os, pybalt, yaml, aiofiles, base64
 import random
 from discord.ext import commands
+
+# Loads the .env file and the data inside of it
+
 load_dotenv(".env")
 botToken = os.getenv('TOKEN')
-bot = commands.Bot(os.getenv("PREFIX"),intents=discord.Intents.all())
+botPrefix = os.getenv("PREFIX")
+bot = commands.Bot(command_prefix=botPrefix,intents=discord.Intents.all())
+
+# Using on_ready() so if the bot runs the user knows its running
+
 @bot.event
 async def on_ready():
     print(f"{bot.user} has woke up from the multibot grave!")
+
 @bot.command(name="ping",description="Used to check the bot's ping to Discord")
 async def ping(ctx):
     await ctx.send(f"Ping: {int(round(bot.latency*1000,0))}ms")
+
 @bot.command(name="random",description="Chooses a random from a range")
 async def ran(ctx, first:int, last:int):
-    await ctx.send(f"From {first} to {last}, I choose {ran.randint(first,last)}!")
+    await ctx.send(f"From {first} to {last}, I choose {random.randint(first,last)}!")
+
 @bot.command(name="download",description="Downloads a video from YouTube/Instagram/Twitter/TikTok")
 async def download(ctx, url):
     path = await pybalt.download(url, videoQuality='480')
@@ -23,8 +35,12 @@ async def download(ctx, url):
     elif "tiktok" in url: await ctx.send("Your T witter video has been downloaded!",file=discord.File(path))
     if os.name == "nt": os.system(f"del {path}") 
     else: os.system(f"rm {path}")
+
 @bot.command(name="encodeb64",description=f"Encodes a string in Base64")
 async def encodeb64(ctx, *, string):
     encoded = base64.b64encode(string.encode()).decode()
     await ctx.send(f"`{string}` encoded in Base64 is `{encoded}`.")
+
+# Runs the bot with the token from .env
+
 bot.run(botToken)
